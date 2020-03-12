da68776b25a1c258a723a3bbddbe1680837ab4ed
Emil <amionv99@gmail.com>
2020-03-12 17:12:22 +0300

    :green_heart: fix test
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_template/config/docs.py
+++ erpnext_template/config/docs.py
@@ -9,3 +9,7 @@ Configuration for docs.
 
 def get_context(context):
 	context.brand_html = "ERPNext Template"
+	context.source_link = "https://github.com/Monogramm/erpnext_template"
+	context.docs_base_url = "https://monogramm.github.io/erpnext_template"
+	context.headline = "TODO_APP_DESCRIPTION"
+	context.sub_heading = "TODO_APP_USAGE"
